d1e008a1705b309246d3ef53aaa9c7fefa346514
raybishal <raybishal.0702@gmail.com>
2022-09-29 14:01:08 +0530

    Movie Info Website Rpoject

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Made with ❤️ by BISHAL RANJAN RAY
+        </div>
+    );
+}
+
+export default Footer;
